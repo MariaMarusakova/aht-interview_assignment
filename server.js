a6afebd9e2a1ce1984a3cb3eea85aff3bd905f95
MariaMarusakova <maruska.marusakova@gmail.com>
2023-05-21 11:32:49 +0200

    create POST method
    - do not accept other method types
    
    - return the received body message

--- server.js
+++ server.js
@@ -3,20 +3,39 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {
-	res.setHeader("Content-Type", "application/json");
-	switch (req.url){
-			case "/telemetry":
-		            res.writeHead(200);
-			    res.end(JSON.stringify({message:"Telemetry Endpoint found"}));
-	                   break
-			default: 
-			   res.writeHead(404);
-			   res.end(JSON.stringify({error:"Source not found"}));
+const requestListener = function(request, response) {
+	response.setHeader("Content-Type", "application/json");
+	switch (request.url) {
+		case "/telemetry":
+
+			if (request.method == 'POST') {
+
+				var body = ''
+				request.on('data', function(data) {
+					body += data
+				})
+				request.on('end', function() {
+					response.writeHead(200, {
+						'Content-Type': 'application/json'
+					})
+					response.end('Received: ' + body)
+				})
+			} else {
+				response.end(JSON.stringify({
+					message: "Telemetry Endpoint found, but I only accept POST method"
+				}));
+			}
+
+			break
+		default:
+			response.writeHead(404);
+			response.end(JSON.stringify({
+				error: "Source not found"
+			}));
 	}
 }
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+	console.log(`Server is running on http://${host}:${port}`);
 });
