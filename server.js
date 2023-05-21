a60fc06344878c10e50c5bc2cb79843082b7f9dd
MariaMarusakova <maruska.marusakova@gmail.com>
2023-05-21 18:59:57 +0200

    add schema to validate the POST request entry

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ const {pool} = require("./db");
 const host = 'localhost';
 const port = 8000;
 
+// Ajv library is used to validate data against json schema
+const Ajv = require("ajv");
+const telemetrySchema = {
+    type: "object",
+    properties: {
+        locationID: {type: "number"},
+        locationAddress: {type: "string"},
+        currentTemp: {type: "number"},
+        status: {type: "boolean"}
+    },
+    required: ["locationID", "locationAddress", "currentTemp"],
+};
+
 const requestListener = function(request, response) {
     response.setHeader("Content-Type", "application/json");
+    const ajv = new Ajv();
     switch (request.url) {
         case "/telemetry":
 
@@ -18,28 +32,46 @@ const requestListener = function(request, response) {
                 })
                 request.on('end', function() {
 
-                    const parsed_request = JSON.parse(body);
-                    try {
-                        pool.query(
-                            "INSERT INTO measured_data (locationID, locationAddress,currentTemp) VALUES ($1, $2, $3)",
-                            [parsed_request.locationID, parsed_request.locationAddress, parsed_request.currentTemp]
-                        );
-                        console.log(`Added a data point. LocationId: ${parsed_request.locationID}, locationAddress: ${parsed_request.locationAddress}, currentTemp: ${parsed_request.currentTemp}`);
-                    } catch (error) {
-                        console.error(error)
+                    // run data against schema defined in telemetrySchema.js
+                    const parsedRequest = JSON.parse(body);
+                    const isValidEntry = ajv.validate(telemetrySchema, parsedRequest);
+                    if (isValidEntry) {
+                        console.log("Valid POST request.");
+
+                        //insert into DB
+
+                        try {
+                            pool.query(
+                                "INSERT INTO measured_data (locationID, locationAddress,currentTemp) VALUES ($1, $2, $3)",
+                                [parsedRequest.locationID, parsedRequest.locationAddress, parsedRequest.currentTemp]
+                            );
+                            console.log(`Added a data point. LocationId: ${parsedRequest.locationID}, locationAddress: ${parsedRequest.locationAddress}, currentTemp: ${parsedRequest.currentTemp}`);
+                        } catch (error) {
+                            console.error(error)
+                        }
+
+                        response.writeHead(200, {
+                            'Content-Type': 'application/json'
+                        })
+                        response.end('Received: ' + body)
+
+
+
+                    } else {
+                        console.error("Invalid POST request. Errors:", ajv.errors);
+                        response.writeHead(404);
+                        response.end(JSON.stringify({
+                            error: "Data has the wrong format."
+                        }));
                     }
 
-                    response.writeHead(200, {
-                        'Content-Type': 'application/json'
-                    })
-                    response.end('Received: ' + body)
                 })
 
 
 
             } else {
                 response.end(JSON.stringify({
-                    message: "Telemetry Endpoint found, but I only accept POST method"
+                    message: "Telemetry Endpoint found, but I only accept POST method."
                 }));
             }
 
